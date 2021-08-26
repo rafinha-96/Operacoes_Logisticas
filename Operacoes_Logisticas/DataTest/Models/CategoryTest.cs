e34b45036b874b35fd618f34c130f579ed485d46
Bruno Gabriel <celilvr13@gmail.com>
2021-08-26 13:07:34 -0300

    Criando Testes para a casse Categoria

--- Operacoes_Logisticas/DataTest/Models/CategoryTest.cs
+++ Operacoes_Logisticas/DataTest/Models/CategoryTest.cs
@@ -10,6 +10,7 @@ namespace DataTest.Models
 {
     public class CategoryTest
     {
+        [Fact]
         public void TestInstanceClassCategory()
         {
             //Arrange
@@ -18,9 +19,55 @@ namespace DataTest.Models
             Type typeCategory;
             //Act
             categoryInstance = Activator.CreateInstance<Category>();
-            typeCategory = compareType.GetType();
+            typeCategory = categoryInstance.GetType();
             //Assert
-            Assert.Equal(typeCategory, compareType);
+            Assert.Equal(compareType, typeCategory);
+        }
+        [Fact]
+        public void TestingHeritageCategory()
+        {
+            //Arrange
+            Category categoryInstance;
+            //Act
+            categoryInstance = new Category();
+            //Assert
+            Assert.IsAssignableFrom<BaseModel>(categoryInstance);
+        }
+        [Fact]
+        public void TestingIdCategory()
+        {
+            //Arrange
+            Category instaceCategory = new Category();
+            int idSetado = 10;
+            //Act
+            instaceCategory.Id = idSetado;
+            //Assert
+            Assert.Equal(idSetado,instaceCategory.Id);
+            Assert.IsType<int>(instaceCategory.Id);
+        }
+        [Fact]
+        public void TestingNameCategory()
+        {
+            //Arrange
+            Category instaceCategory = new Category();
+            string NameSetado = "Nome";
+            //Act
+            instaceCategory.Name = NameSetado;
+            //Assert
+            Assert.Equal(NameSetado, instaceCategory.Name);
+            Assert.IsType<string>(instaceCategory.Name);
+        }
+        [Fact]
+        public void TestingDescriptionCategory()
+        {
+            //Arrange
+            Category instaceCategory = new Category();
+            string DescriptionSetado = "Description";
+            //Act
+            instaceCategory.Description = DescriptionSetado;
+            //Assert
+            Assert.Equal(DescriptionSetado, instaceCategory.Description);
+            Assert.IsType<string>(instaceCategory.Description);
         }
     }
 }
