50b5f96b232e2a00b8e73ee449303be8294fe648
Bruno Gabriel <celilvr13@gmail.com>
2021-08-26 12:55:10 -0300

    Criando Testes para a casse Produto

---
+++ Operacoes_Logisticas/DataTest/Models/CategoryTest.cs
@@ -0,0 +1,26 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DataTest.Models
+{
+    public class CategoryTest
+    {
+        public void TestInstanceClassCategory()
+        {
+            //Arrange
+            Category categoryInstance;
+            Type compareType = typeof(Category);
+            Type typeCategory;
+            //Act
+            categoryInstance = Activator.CreateInstance<Category>();
+            typeCategory = compareType.GetType();
+            //Assert
+            Assert.Equal(typeCategory, compareType);
+        }
+    }
+}
