1432cf9dcd8c756e932a817d3138eeb95ad81d74
caio <caio.mateus.r@gmail.com>
2021-08-26 12:57:04 -0300

    StoreTest created

---
+++ Operacoes_Logisticas/DataTest/Models/StoreTest.cs
@@ -0,0 +1,44 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DataTest.Models
+{
+    public class StoreTest
+    {
+        //arrange
+        //act
+        //assert
+
+        [Fact]
+        public void TestingStoreHeritage()
+        {
+            //assert
+            Assert.IsAssignableFrom<BaseModel>(new Store());
+        }
+
+        [Fact]
+        public void TestingStoreProperties()
+        {
+            //arrange
+            Store store = new Store();
+            //act
+            int idtest = 10;
+            string nametest = "TestStore";
+            int quantitytest = 20;
+
+            store.Id = idtest;
+            store.Name = nametest;
+            store.Quantity = quantitytest;
+            //assert
+            Assert.Equal(idtest, store.Id);
+            Assert.Equal(nametest, store.Name);
+            Assert.Equal(quantitytest, store.Quantity);
+
+        }
+    }
+}
