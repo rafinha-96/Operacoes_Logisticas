dca7e6bf1fea69a3288712598489ea65a25e22e0
Vinicius Vendrame <vini.vendrame13@gmail.com>
2021-08-26 12:21:57 -0300

    Created ProviderHistory model

---
+++ Operacoes_Logisticas/Data/Models/ProviderHistory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class ProviderHistory : BaseModel
+    {
+        public DateTime Date { get; set; }
+        //public ProviderRequest ProviderRequest { get; set; }
+    }
+}
