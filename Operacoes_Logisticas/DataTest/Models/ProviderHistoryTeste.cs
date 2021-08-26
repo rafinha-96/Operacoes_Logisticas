59de1d77e21c48f9bf1e3b494ba9fe4fce2786fc
Vinicius Vendrame <vini.vendrame13@gmail.com>
2021-08-26 14:14:59 -0300

    ProviderHistoryTest

--- Operacoes_Logisticas/DataTest/Models/ProviderHistoryTeste.cs
+++ Operacoes_Logisticas/DataTest/Models/ProviderHistoryTeste.cs
@@ -1,12 +1,72 @@
-﻿using System;
+﻿using Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace DataTest.Models
 {
-    class ProviderHistoryTeste
+    public class ProviderHistoryTeste
     {
+        [Fact]
+        public void TestingProviderHistoryInstance()
+        {
+            //Arrange
+            ProviderHistory model;
+            //Act
+            model = new ProviderHistory();
+            //Assert
+            Assert.IsType<ProviderHistory>(model);
+        }
+
+        [Fact]
+        public void TestingProviderHistoryHeritage()
+        {
+            //Arrange
+            Type baseModelType = typeof(BaseModel);
+            Type providerHistoryType = typeof(ProviderHistory);
+            //Act
+            bool inherits = providerHistoryType.IsSubclassOf(baseModelType);
+            //Assert
+            Assert.True(inherits);
+        }
+
+        [Fact]
+        public void TestingProviderHistoryIdProp()
+        {
+            //Arrange
+            ProviderHistory model = new ProviderHistory();
+            int id = 1;
+            //Act
+            model.Id = id;
+            //Assert
+            Assert.IsType<int>(model.Id);
+            Assert.Equal(id, model.Id);            
+        }
+
+        [Fact]
+        public void TestingProviderHistoryDateTimeProp()
+        {
+            //Arrange
+            ProviderHistory model = new ProviderHistory();
+            DateTime date = DateTime.Today;
+            //Act
+            model.Date = date;
+            //Assert
+            Assert.IsType<DateTime>(model.Date);
+            Assert.Equal(date, model.Date);
+        }
+
+        [Fact]
+        public void TestingProviderHistoryComposition()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
     }
 }
