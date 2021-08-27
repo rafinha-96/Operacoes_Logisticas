1d56161b2d9fcc3d4b94e89fc4a301ef464e51c0
caio <caio.mateus.r@gmail.com>
2021-08-27 10:00:30 -0300

    All tests for Product property of StoreRequest class, done

--- Operacoes_Logisticas/DataTest/Models/StoreRequestTest/ProductPropertyStoreRequestTest.cs
+++ Operacoes_Logisticas/DataTest/Models/StoreRequestTest/ProductPropertyStoreRequestTest.cs
@@ -11,5 +11,27 @@ namespace DataTest.Models.StoreRequestTest
 {
     public class ProductPropertyStoreRequestTest
     {
+        [Fact]
+        public void TestingStorePropertyExists()
+        {
+            //Arrange
+            Type t = typeof(StoreRequest);
+            //Act
+            PropertyInfo pI = t.GetProperties().FirstOrDefault(p => p.Name == "Product");
+            //Assert
+            Assert.NotNull(pI);
+        }
+        [Fact]
+        public void TestingStorePropertyComposition()
+        {
+            //Arrange
+            Type t = typeof(StoreRequest);
+            Type tproduct = typeof(Product);
+            //Act
+            PropertyInfo pI = t.GetProperties().FirstOrDefault(p => p.Name == "Store");
+            Type typeProperty = pI != null ? pI.PropertyType : null;
+            //Assert
+            Assert.Equal(tproduct, typeProperty);
+        }
     }
 }
