130f333cabd155952128e5edc494cd923e6be8de
Alex Junior <alexjuniorkuss@outlook.com>
2021-08-27 13:38:41 -0300

    some alterations

---
+++ Operacoes_Logisticas/DataTest/Models/Contacts/ContactTest.cs
@@ -0,0 +1,23 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DataTest.Models.Contacts
+{
+    public class StatusClass : BaseClassModelTest<Contact>
+    {
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        [Fact]
+        public void TestingInheritsContactOfBase()
+        {
+            Contact contt;
+            contt = Activator.CreateInstance<Contact>();
+            Assert.IsAssignableFrom<BaseModel>(contt);
+        }
+    }
+}
