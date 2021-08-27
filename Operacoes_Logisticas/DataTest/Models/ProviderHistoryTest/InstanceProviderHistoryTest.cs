c3e29c510812b0722a05c151128ea64f9ad1e0c1
Vinicius Vendrame <vini.vendrame13@gmail.com>
2021-08-27 13:07:50 -0300

    Updating tests

---
+++ Operacoes_Logisticas/DataTest/Models/ProviderHistoryTest/InstanceProviderHistoryTest.cs
@@ -0,0 +1,24 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DataTest.Models.ProviderHistoryTest
+{
+    public class InstanceProviderHistoryTest
+    {
+        [Fact]
+        public void TestingProviderHistoryInstance()
+        {
+            //Arrange
+            ProviderHistory model = new ProviderHistory();
+            //Act
+
+            //Assert
+            Assert.IsType<ProviderHistory>(model);
+        }
+    }
+}
