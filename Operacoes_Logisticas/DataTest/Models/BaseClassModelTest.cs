fa2b8d8919742e4fd484775605cb894de5579aa5
PauloSala <paulohsala@gmail.com>
2021-08-27 12:34:42 -0300

    Adicionando um teste model genérico

---
+++ Operacoes_Logisticas/DataTest/Models/BaseClassModelTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DataTest.Models
+{
+    public abstract class BaseClassModelTest<ClassTest>
+    {
+        [Fact]
+        public void TestInstanceClassProviderRequest()
+        {
+            ClassTest instance;
+
+            instance = Activator.CreateInstance<ClassTest>();
+
+            Assert.IsType<ClassTest>(instance);
+        }
+        [Fact]
+        public void AllPropTest()
+        {           
+            foreach (var prop in this.GetType().GetProperties())
+            {
+                var a =  new BasePropTest(prop.Name,prop.PropertyType,typeof(ClassTest));
+            }
+        }
+    }
+}
--- Operacoes_Logisticas/DataTest/Models/BasePropTest.cs
+++ Operacoes_Logisticas/DataTest/Models/BasePropTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -9,32 +10,36 @@ using Xunit;
 
 namespace DataTest.Models
 {
-    public abstract class BasePropTest<T, _typeProp>
+    public class BasePropTest
     {
         private string _nameProp;
-        public BasePropTest(string name)
+        private Type _typeProp { get; set; }
+        private Type _ClassType { get; set; }
+        public BasePropTest(string nameProp, Type typeProp, Type classType)
         {
-            _nameProp = name;
+            _nameProp = nameProp;
+            _typeProp = typeProp;
+            _ClassType = classType;
+            TestPropExist();
+            TestTypeProp();
+            TestGetSetProp();
         }
-        [Fact]
         public void TestPropExist()
         {
-            Type t = typeof(T);
+            Type t = _ClassType;
             PropertyInfo pI = t.GetProperties().FirstOrDefault(p => p.Name == _nameProp);
             Assert.NotNull(pI);
         }
-        [Fact]
         public void TestTypeProp()
         {
-            Type t = typeof(T);
+            Type t = _ClassType;
             PropertyInfo pI = t.GetProperties().FirstOrDefault(p => p.Name == _nameProp);
-            Assert.Equal(typeof(_typeProp),pI != null ? pI.PropertyType : null);
+            Assert.Equal(_typeProp,pI != null ? pI.PropertyType : null);
         }
-        [Fact]
         public void TestGetSetProp()
         {
-            T s = Activator.CreateInstance<T>();
-            Type t = typeof(_typeProp);
+            var s = Activator.CreateInstance(_ClassType);
+            Type t = _typeProp;
             PropertyInfo pI = s.GetType().GetProperties().FirstOrDefault(p => p.Name == _nameProp);
 
             object valueProp = null;
@@ -45,7 +50,7 @@ namespace DataTest.Models
                     pI.SetValue(s, ""); 
                 }else 
                 { 
-                    pI.SetValue(s, Activator.CreateInstance(typeof(_typeProp))); 
+                    pI.SetValue(s, Activator.CreateInstance(_typeProp)); 
                 }
                 valueProp = pI.GetValue(s);
             }
