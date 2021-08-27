1bb8875306afa3889cb5d6417d643f0b5304d3f9
Bruno Gabriel <celilvr13@gmail.com>
2021-08-27 13:41:33 -0300

    Criando Pasta CategoryTest e editando props do model Category

---
+++ Operacoes_Logisticas/DataTest/Models/CategoryTest/CategoryTest.cs
@@ -0,0 +1,37 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DataTest.Models.CategoryTest
+{
+    public class CategoryTest
+    {
+        [Fact]
+        public void TestInstanceClassCategory()
+        {
+            //Arrange
+            Category categoryInstance;
+            Type compareType = typeof(Category);
+            Type typeCategory;
+            //Act
+            categoryInstance = Activator.CreateInstance<Category>();
+            typeCategory = categoryInstance.GetType();
+            //Assert
+            Assert.Equal(compareType, typeCategory);
+        }
+        [Fact]
+        public void TestingHeritageCategory()
+        {
+            //Arrange
+            Category categoryInstance;
+            //Act
+            categoryInstance = new Category();
+            //Assert
+            Assert.IsAssignableFrom<BaseModel>(categoryInstance);
+        }
+    }
+}
---
+++ Operacoes_Logisticas/DataTest/Models/CategoryTest/PropIdClassCategoryTest.cs
@@ -0,0 +1,54 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DataTest.Models.CategoryTest
+{
+    public class PropIdClassCategoryTest
+    {
+        [Fact]
+        public void TestPropIdExisist()
+        {
+            //Arrange
+            Type instanceCategory = typeof(Category);
+            //Act
+            PropertyInfo idInfo = instanceCategory.GetProperties().FirstOrDefault(i => i.Name == "Id");
+            //Assert
+            Assert.NotNull(idInfo);
+        }
+        [Fact]
+        public void TestPropIdIsInt()
+        {
+            //Arrange
+            Type instanceCategory = typeof(Category);
+            Type typeString = typeof(int);
+            PropertyInfo idInfo = instanceCategory.GetProperties().FirstOrDefault(i => i.Name == "Id");
+            //Act
+            Type typeCategory = idInfo != null ? idInfo.PropertyType : null;
+            //Assert
+            Assert.Equal(typeString, typeCategory);
+        }
+        [Fact]
+        public void TestPropIdGet()
+        {
+            //Arrange
+            Type instanceType = typeof(Category);
+            Category instanceCategory = Activator.CreateInstance<Category>();
+            PropertyInfo idInfo = instanceType.GetProperties().FirstOrDefault(i => i.Name == "Id");
+            object valueProp = null;
+            //Act
+            if (idInfo != null && idInfo.PropertyType == typeof(int))
+            {
+                idInfo.SetValue(instanceCategory, 0);
+                valueProp = idInfo.GetValue(instanceCategory);
+            }
+            //Asserts
+            Assert.NotNull(valueProp);
+        }
+    }
+}
